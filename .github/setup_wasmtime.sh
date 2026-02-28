ed631c3597850b0be4d73d5d1d67b62da508aa61
Jan Verbeek <jan.verbeek@posteo.nl>
2026-02-28 14:40:58 +0100

    CI: Update wasmtime

--- .github/setup_wasmtime.sh
+++ .github/setup_wasmtime.sh
@@ -6,7 +6,7 @@ if test -z "$RUNNER_OS"; then
     exit 1
 fi
 
-url=https://github.com/bytecodealliance/wasmtime/releases/download/v31.0.0/wasmtime-v31.0.0-x86_64-linux.tar.xz
+url=https://github.com/bytecodealliance/wasmtime/releases/download/v42.0.1/wasmtime-v42.0.1-x86_64-linux.tar.xz
 
 cd /tmp
 curl -L "$url" | tar Jx
